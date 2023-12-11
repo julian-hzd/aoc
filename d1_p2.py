cb2538ce8e4383ca4507937fbdfcf2b1ca2badc7
julian-hzd <julianhzd@hotmail.com>
2023-12-11 16:07:58 -0500

    final solution, rfind for last number occurrence

--- d1_p2.py
+++ d1_p2.py
@@ -13,37 +13,57 @@ numbers = {
 
 def main():
     total = 0
+    # print(line_value("1threenine241gnrdfqn5"))
+    # return
 
-    # with open("d1.txt", "r") as f:
-    #     for l in f:
-    #         total += line_value(l)
+    with open("d1.txt", "r") as f:
+        for l in f:
+            x = line_value(l)
+            print(x)
+            total += x
 
-    print(line_value("kjony1kjoneoptwo"))
+    print(total)
 
 
 def line_value(line: str) -> int:
+    first_num = -1
+    second_num = -1
     large_num = 999999
-    m: int = large_num
+    min_pos: int = large_num
+    max_pos: int = -1
 
     for _str, _int in numbers.items():
-        str_pos = line.find(_str)  # 'one'
-        str_pos = str_pos if str_pos != -1 else large_num
+        print(_int)
+        # if the number was not found then
+        # assign 999999 to ignore it in the min()
 
+        str_pos = line.find(_str)  # 'one'
         num_pos = line.find(str(_int))
+
+        str_pos = str_pos if str_pos != -1 else large_num
         num_pos = num_pos if num_pos != -1 else large_num
 
-        print(f"str pos {str_pos}")
-        print(f"num pos {num_pos}")
-        print("\n")
+        # if new minimum position save the new number
+        temp = min_pos
+        min_pos = min(str_pos, num_pos, min_pos)
+        first_num = _int if temp != min_pos else first_num
 
-        m = min(str_pos, num_pos, min)
+        # if the number was not found then
+        # assign -1 to ignore it in the max()
 
-        # if num_pos != -1:
-        #     min = num_pos if num_pos < min else min
-        # if str_pos != -1:
-        #     min = str_pos if str_pos < num_pos else num_pos  # '1'
+        str_pos = line.rfind(_str)  # 'one'
+        num_pos = line.rfind(str(_int))
 
-    return min
+        str_pos = str_pos if str_pos != large_num else -1
+        num_pos = num_pos if num_pos != large_num else -1
+
+        # if new max position save the new number
+        temp = max_pos
+        max_pos = max(str_pos, num_pos, max_pos)
+        second_num = _int if temp != max_pos else second_num
+        print(f"{str(first_num)}{str(second_num)}")
+
+    return int(f"{str(first_num)}{str(second_num)}")
 
 
 main()
