881898db7f05022b5f3c910bdf1cb2ac08a6f526
julian-hzd <julianhzd@hotmail.com>
2023-12-11 14:28:52 -0500

    d1 p2 start

--- d1_p2.py
+++ d1_p2.py
@@ -22,18 +22,26 @@ def main():
 
 
 def line_value(line: str) -> int:
-    min: int = 999999
+    large_num = 999999
+    m: int = large_num
+
     for _str, _int in numbers.items():
         str_pos = line.find(_str)  # 'one'
+        str_pos = str_pos if str_pos != -1 else large_num
+
         num_pos = line.find(str(_int))
+        num_pos = num_pos if num_pos != -1 else large_num
+
         print(f"str pos {str_pos}")
         print(f"num pos {num_pos}")
         print("\n")
 
-        if num_pos != -1:
-            min = num_pos if num_pos < min else min
-        if str_pos != -1:
-            min = str_pos if str_pos < num_pos else num_pos  # '1'
+        m = min(str_pos, num_pos, min)
+
+        # if num_pos != -1:
+        #     min = num_pos if num_pos < min else min
+        # if str_pos != -1:
+        #     min = str_pos if str_pos < num_pos else num_pos  # '1'
 
     return min
 
